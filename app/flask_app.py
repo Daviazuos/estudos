e92ba891cce4734d9d80b778d10d261c47dca994
davi <davib.souza@hotmail.com>
2019-06-15 19:13:27 +0000

    agora na nuvem

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
 
-app = Flask(__name__)
+from flask import Flask,render_template
 
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def home():
